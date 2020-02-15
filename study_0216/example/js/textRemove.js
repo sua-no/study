fce057a22549942cf81868d702e26bc8c34f90a0
sua-no <60374596+sua-no@users.noreply.github.com>
2020-02-16 00:39:08 +0900

    스터디 준비

---
+++ study_0216/example/js/textRemove.js
@@ -0,0 +1,13 @@
+let i = 0;
+let j = 0;
+const textArray = ["HTML", "CSS", "Javascript"],
+    speed = 100,
+    target = document.getElementById("type");
+
+
+//한글자씩 추출해서 html에 출력 charAt
+function type(){}
+//한글자씩 html에서 제거 slice
+function remove(){}
+//다음 문자열로 넘어감
+function textNum(){}
