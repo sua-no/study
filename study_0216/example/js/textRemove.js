5b32a5862cd3cf070d529d390291bd206d7d64ab
sua-no <60374596+sua-no@users.noreply.github.com>
2020-02-16 17:35:21 +0900

    0216스터디끝

--- study_0216/example/js/textRemove.js
+++ study_0216/example/js/textRemove.js
@@ -4,10 +4,34 @@ const textArray = ["HTML", "CSS", "Javascript"],
     speed = 100,
     target = document.getElementById("type");
 
+type();
 
 //한글자씩 추출해서 html에 출력 charAt
-function type(){}
+function type(){
+    if(i < textArray[j].length){
+        (target.innerHTML += textArray[j].charAt(i));
+        i++;
+        setTimeout(type, speed);
+    }else{
+        remove();
+    }
+}
 //한글자씩 html에서 제거 slice
-function remove(){}
+function remove(){
+    if(0 <= i){
+        (target.innerHTML = textArray[j].slice(0,i));
+        i--;
+        setTimeout(remove, speed);
+    }else{
+        type();
+        textNum();
+    }
+}
 //다음 문자열로 넘어감
-function textNum(){}
+function textNum(){
+    if(j == textArray.length - 1){
+        j=0;
+    }else{
+        j++;
+    }
+}
